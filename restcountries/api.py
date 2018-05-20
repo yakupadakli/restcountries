54cde5eca73551f09d12eb24adb9f52f2e56e7d7
Yakup Adakli <yakup.adakli@gmail.com>
2018-05-20 16:12:31 +0300

    fields parameter added for all endpoints.

--- restcountries/api.py
+++ restcountries/api.py
@@ -18,32 +18,32 @@ class RestCountries(object):
     def _country(self):
         return Country(api=self)
 
-    def all(self):
-        return self._country.all()
+    def all(self, fields=()):
+        return self._country.all(fields=fields)
 
-    def name(self, name, full_text=False):
-        return self._country.name(name, full_text=full_text)
+    def name(self, name, full_text=False, fields=()):
+        return self._country.name(name, full_text=full_text, fields=fields)
 
-    def code(self, code):
-        return self._country.code(code)
+    def code(self, code, fields=()):
+        return self._country.code(code, fields=fields)
 
-    def code_list(self, code_list):
-        return self._country.code_list(code_list)
+    def code_list(self, code_list, fields=()):
+        return self._country.code_list(code_list, fields=fields)
 
-    def currency(self, currency):
-        return self._country.currency(currency)
+    def currency(self, currency, fields=()):
+        return self._country.currency(currency, fields=fields)
 
-    def language(self, language_code):
-        return self._country.language(language_code)
+    def language(self, language_code, fields=()):
+        return self._country.language(language_code, fields=fields)
 
-    def capital(self, capital):
-        return self._country.capital(capital)
+    def capital(self, capital, fields=()):
+        return self._country.capital(capital, fields=fields)
 
-    def calling_code(self, calling_code):
-        return self._country.calling_code(calling_code)
+    def calling_code(self, calling_code, fields=()):
+        return self._country.calling_code(calling_code, fields=fields)
 
-    def region(self, region):
-        return self._country.region(region)
+    def region(self, region, fields=()):
+        return self._country.region(region, fields=fields)
 
-    def regional_bloc(self, regional_bloc):
-        return self._country.regional_bloc(regional_bloc)
+    def regional_bloc(self, regional_bloc, fields=()):
+        return self._country.regional_bloc(regional_bloc, fields=fields)
--- restcountries/country.py
+++ restcountries/country.py
@@ -7,109 +7,126 @@ class Country(Client):
 
     def __init__(self, **kwargs):
         super(Country, self).__init__(**kwargs)
+        self.fields_tag = "fields"
 
-    def _country_list(self, url, params=None):
+    def _country_list(self, url, params=None, fields=()):
+        params = params or {}
+        params.update(self._get_filter_params(fields))
         result = self._get(url, params=params)
         return CountryModel._parse_list(result)
 
-    def _country_get(self, url, params=None):
+    def _country_get(self, url, params=None, fields=()):
+        params = params or {}
+        params.update(self._get_filter_params(fields))
         result = self._get(url, params=params)
         return CountryModel._parse(result)
 
-    def all(self):
+    def _get_filter_params(self, fields):
+        return {self.fields_tag: ";".join(fields)}
+
+    def all(self, fields=()):
         """
         Get all list of countries.
 
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/all"
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def name(self, name, full_text=False):
+    def name(self, name, full_text=False, fields=()):
         """
         Search by country name. It can be the native name or partial name.
 
         :param name: Country name.
         :param full_text: Search by country full name.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/name/%s" % name
         params = {"fullText": full_text}
-        return self._country_list(url, params=params)
+        return self._country_list(url, params=params, fields=fields)
 
-    def code(self, code):
+    def code(self, code, fields=()):
         """
         Search by ISO 3166-1 2-letter or 3-letter country code.
 
         :param code: Country code.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/alpha/%s" % code
-        return self._country_get(url)
+        return self._country_get(url, fields=fields)
 
-    def code_list(self, code_list):
+    def code_list(self, code_list, fields=()):
         """
         Search by list of ISO 3166-1 2-letter or 3-letter country codes.
 
         :param code_list: Country code list.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/alpha"
         codes = ";".join(code_list)
         params = {"codes": codes}
-        return self._country_list(url, params=params)
+        return self._country_list(url, params=params, fields=fields)
 
-    def currency(self, currency_code):
+    def currency(self, currency_code, fields=()):
         """
         Search by ISO 4217 currency code.
 
         :param currency_code: Currency code.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/currency/%s" % currency_code
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def language(self, language_code):
+    def language(self, language_code, fields=()):
         """
         Search by ISO 4217 currency code.
 
         :param language_code: Language code.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/lang/%s" % language_code
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def capital(self, capital):
+    def capital(self, capital, fields=()):
         """
         Search by capital city.
 
         :param capital: Capital city.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/capital/%s" % capital
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def calling_code(self, calling_code):
+    def calling_code(self, calling_code, fields=()):
         """
         Search by calling code.
 
         :param calling_code: Calling code.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/callingcode/%s" % calling_code
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def region(self, region):
+    def region(self, region, fields=()):
         """
         Search by region: Africa, Americas, Asia, Europe, Oceania.
 
         :param region: Region.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/region/%s" % region
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
 
-    def regional_bloc(self, regional_bloc):
+    def regional_bloc(self, regional_bloc, fields=()):
         """
         Search by regional bloc:
             EU (European Union)
@@ -127,7 +144,8 @@ class Country(Client):
             SAARC (South Asian Association for Regional Cooperation)
 
         :param regional_bloc: Regional bloc.
+        :param fields: Filter the output of your request to include only the specified fields.
         :return [List]: Country list.
         """
         url = "/regionalbloc/%s" % regional_bloc
-        return self._country_list(url)
+        return self._country_list(url, fields=fields)
