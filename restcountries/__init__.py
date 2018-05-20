903a83f9683d7a725b48a79c30dcde747e90d736
Yakup Adaklı <yakup.adakli@gmail.com>
2018-05-20 14:29:50 +0300

    Initial commit

---
+++ restcountries/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+# Unsplash
+# Copyright 2018 Yakup Adaklı
+# See LICENSE for details.
+
+"""A library that provides a Python wrapper to the RestCountries API"""
+
+__version__ = "1.0.0b1"
+__author__ = "Yakup Adaklı"
+__license__ = "MIT"
