1d0f9cdfbef14c941f047830e0f60bca96bfe301
Yakup Adakli <yakup.adakli@gmail.com>
2020-08-19 18:24:13 +0300

    Version update.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="restcountries",
-    version="1.1.1",
+    version="1.1.2",
     description="A Python client for the RestCountries API.",
     license="MIT",
     author="Yakup Adaklı",
